dc6d73f882a5daf159999bfecd66dc3a33b14e75
JsuisSayker <killian.trouve@epitech.eu>
2024-03-20 13:47:30 +0100

    [UPD] CMakeLists.txt
    change the CMakeLists.txt and add some include files

---
+++ include/NCurses.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** Arcade
+** File description:
+** NCurses
+*/
+
+#ifndef NCURSES_HPP_
+#define NCURSES_HPP_
+
+class NCurses {
+    public:
+        NCurses();
+        ~NCurses();
+
+    protected:
+    private:
+};
+
+#endif /* !NCURSES_HPP_ */
---
+++ include/Pacman.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2024
+** Arcade
+** File description:
+** Pacman
+*/
+
+#ifndef PACMAN_HPP_
+#define PACMAN_HPP_
+
+class Pacman {
+    public:
+        Pacman();
+        ~Pacman();
+
+    protected:
+    private:
+};
+
+#endif /* !PACMAN_HPP_ */
