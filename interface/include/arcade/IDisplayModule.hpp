00c1688960d09bdd8d9ac4b392eb40111b693318
JsuisSayker <killian.trouve@epitech.eu>
2024-03-28 20:39:19 +0100

    [ADD] interface
    add some hpp file in the interface folder

---
+++ interface/include/arcade/IDisplayModule.hpp
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2024
+** bsArcade
+** File description:
+** IDisplayModule
+*/
+
+#ifndef IDISPLAYMODULE_HPP_
+#define IDISPLAYMODULE_HPP_
+
+#include "Arcade.hpp"
+#include "CoreModule.hpp"
+namespace arcade {
+class CoreModule;
+class IDisplayModule {
+public:
+  IDisplayModule(){};
+  virtual ~IDisplayModule(){};
+
+  void sendGameData(arcade::GameData data);
+  void sendMenuData(arcade::MenuData data);
+
+  virtual void setCoreModule(arcade::CoreModule *coreModule) = 0;
+  virtual arcade::CoreModule *getCoreModule() const = 0;
+
+  virtual void clearWindow() = 0;
+  virtual void displayWindow() = 0;
+  virtual arcade::KeyboardInput getInput() = 0;
+  virtual void
+  drawSprite(std::pair<char, std::string> sprite, int x, int y, int width, int height) = 0;
+  virtual void drawText(const std::string text, int x, int y, int size) = 0;
+
+protected:
+  arcade::CoreModule *_coreModule;
+};
+}; // namespace arcade
+
+#endif /* !IDISPLAYMODULE_HPP_ */
