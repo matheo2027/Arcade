75219928842ec24e3aadb06a830348478d138aa3
JsuisSayker <killian.trouve@epitech.eu>
2024-03-28 13:25:28 +0100

    [ADD] ADisplayModule.cpp
     add the file

---
+++ lib/graphics/ADisplayModule.cpp
@@ -0,0 +1,85 @@
+/*
+** EPITECH PROJECT, 2024
+** Arcade
+** File description:
+** IDisplayModule
+*/
+
+#include "ADisplayModule.hpp"
+
+/**
+ * @brief Construct a new arcade::ADisplayModule::ADisplayModule object
+ *
+ */
+arcade::ADisplayModule::ADisplayModule() : arcade::IModule(), arcade::IDisplayModule()
+{
+  this->_displayStatus = DisplayStatus::SELECTION;
+}
+
+/**
+ * @brief Destroy the arcade::ADisplayModule::ADisplayModule object
+ *
+ */
+arcade::ADisplayModule::~ADisplayModule() {}
+
+/**
+ * @brief return the type of the module
+ *
+ * @return arcade::IModule::ModuleType
+ */
+arcade::IModule::ModuleType arcade::ADisplayModule::getType() const
+{
+  return arcade::IModule::ModuleType::GRAPHIC;
+}
+
+/**
+ * @brief set the status of the display module
+ *
+ * @param status of the display module
+ */
+void arcade::ADisplayModule::setDisplayStatus(
+    arcade::ADisplayModule::DisplayStatus status)
+{
+  this->_displayStatus = status;
+}
+
+/**
+ * @brief return the status of the display module
+ *
+ * @return arcade::ADisplayModule::DisplayStatus status of the display module
+ */
+arcade::ADisplayModule::DisplayStatus
+arcade::ADisplayModule::getDisplayStatus() const
+{
+  return this->_displayStatus;
+}
+
+/**
+ * @brief receive send data of the game module to the display module
+ *
+ * @param data of the game module (score, map, player position)
+ */
+void arcade::ADisplayModule::sendGameData(arcade::IModule::GameData data)
+{
+  return;
+}
+
+/**
+ * @brief set the core module to the display module
+ *
+ * @param coreModule
+ */
+void arcade::ADisplayModule::setCoreModule(arcade::CoreModule *coreModule)
+{
+  this->_coreModule = coreModule;
+}
+
+/**
+ * @brief get the core module
+ *
+ * @return arcade::CoreModule *
+ */
+arcade::CoreModule *arcade::ADisplayModule::getCoreModule() const
+{
+  return this->_coreModule;
+}
